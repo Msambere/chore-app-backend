c0c86e2d57f2f3cdf87715de8b42ad60b0bdc2a1
Amber <110812972+Msambere@users.noreply.github.com>
2025-01-15 20:12:08 -0600

    First commit

---
+++ src/main/java/ada/chore_api_v2/ChoreApiV2Application.java
@@ -0,0 +1,13 @@
+package ada.chore_api_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoreApiV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoreApiV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ada/chore_api_v2/ChoreApiV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ada.chore_api_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoreApiV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
