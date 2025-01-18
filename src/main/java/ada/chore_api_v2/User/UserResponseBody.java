cb0f56270ad59d8004b81dd566625170ee564722
Amber <110812972+Msambere@users.noreply.github.com>
2025-01-17 20:32:29 -0600

    Create response body objects for user and chore

---
+++ src/main/java/ada/chore_api_v2/User/UserResponseBody.java
@@ -0,0 +1,27 @@
+package ada.chore_api_v2.User;
+
+import ada.chore_api_v2.Chore.Chore;
+import ada.chore_api_v2.Chore.ChoreResponseBody;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserResponseBody {
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String username;
+    public Set<ChoreResponseBody> chores; // Change to choreResponseBody?
+
+
+
+    public UserResponseBody(User user) {
+            this.firstName = user.getFirstName();
+            this.lastName = user.getLastName();
+            this.email = user.getEmail();
+            this.username = user.getUsername();
+            this.chores = user.getChoreResponses();
+            // Add logic to take in chores and convert them to responsebody
+    }
+
+}
