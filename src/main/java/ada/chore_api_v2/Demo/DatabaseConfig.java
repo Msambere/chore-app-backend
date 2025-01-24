f04afd2e8fb5c689c3242460df9a8a7a51ce6675
anhtran <anhtr077@gmail.com>
2025-01-23 17:50:09 -0800

    update Demo DataConfig and update application’s database configuration in application.properties

---
+++ src/main/java/ada/chore_api_v2/Demo/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package ada.chore_api_v2.Demo;
+
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
+
