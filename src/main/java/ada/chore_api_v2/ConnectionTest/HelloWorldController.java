c456df29357bd38275637ee0b643ac72b71e1ab0
Amber <110812972+Msambere@users.noreply.github.com>
2025-01-24 00:22:42 -0600

    Move test root url

--- src/main/java/ada/chore_api_v2/ConnectionTest/HelloWorldController.java
+++ src/main/java/ada/chore_api_v2/ConnectionTest/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/HelloWorld")
+@RequestMapping("")
 public class HelloWorldController {
     public HelloWorldController() {}
 
