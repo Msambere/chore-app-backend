0f90570f50f91f140a25f0bcd7ae432333d5b4ae
Amber <110812972+Msambere@users.noreply.github.com>
2025-02-05 17:59:25 -0600

    Refactor to validate request body

--- src/main/java/ada/chore_api_v2/Chore/ChoreController.java
+++ src/main/java/ada/chore_api_v2/Chore/ChoreController.java
@@ -56,7 +56,7 @@ public class ChoreController {
 
     // Update chore
     @PatchMapping("chores/{choreId}")
-    public ResponseEntity<GenericResponseBody> updateChore(@PathVariable Integer choreId, @RequestBody Chore choreRequest, BindingResult result) {
+    public ResponseEntity<GenericResponseBody> updateChore(@PathVariable Integer choreId, @Valid @RequestBody Chore choreRequest, BindingResult result) {
         if(result.hasErrors()) {
             GenericResponseBody errorResponse = new GenericResponseBody("Invalid request body");
             return new ResponseEntity<GenericResponseBody>(errorResponse, HttpStatus.BAD_REQUEST);
